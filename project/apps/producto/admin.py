4cab1ddc2ff063d6b212ddd68abc4c46ef5478f4
Martin Naveiro <martin.naveiro@gmail.com>
2023-06-09 01:21:26 -0300

    filtros admin

--- project/apps/producto/admin.py
+++ project/apps/producto/admin.py
@@ -1,1 +1,15 @@
+# from django.contrib import admin
+# from .models import Producto
+
+# admin.site.register(Producto)
+
 from django.contrib import admin
+from .models import Producto
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("tipo_vehiculo", "marca", "modelo", "anio")  # Campos a mostrar en la lista
+    list_filter = ("tipo_vehiculo", "marca")  # Filtros de búsqueda
+
+
+admin.site.register(Producto, ProductoAdmin)
--- project/apps/producto/models.py
+++ project/apps/producto/models.py
@@ -2,17 +2,12 @@ from django.db import models
 
 
 class Producto(models.Model):
-    tipo_vehiculo = models.CharField(max_length=100)
+    tipo_vehiculo = models.CharField(max_length=100, unique=True)
     marca = models.CharField(max_length=100)
     modelo = models.CharField(max_length=100)
     anio = models.IntegerField()
-    detalle = models.CharField(max_length=100, default="")  # Nuevo campo para el detalle
-
-    imagen = models.ImageField(upload_to="productos/", blank=True, null=True)
+    detalle = models.TextField()  # Nuevo campo para el detalle
+    imagen = models.ImageField(upload_to="producto_imagenes/")  # Nuevo campo para la imagen
 
     def __str__(self):
         return self.tipo_vehiculo
-
-
-# imagen = models.ImageField(upload_to="producto_imagenes/")  # Nuevo campo para la imagen
-imagen = models.ImageField(upload_to="productos/", blank=True, null=True)
