4115572e316102606ea874e87a2339f8868f33b4
Martin Naveiro <martin.naveiro@gmail.com>
2023-06-10 19:27:39 -0300

    commit login

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -1,9 +1,21 @@
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.urls import path
+# from django.contrib.auth import views as auth_views
+# from . import views
+
+# urlpatterns = [
+#   path("", views.index, name="index"),
+#  path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+# ]
+# urlpatterns += staticfiles_urlpatterns()
+
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", auth_views.LoginView.as_view(template_name="home/login.html"), name="login"),
 ]
 urlpatterns += staticfiles_urlpatterns()
--- project/config/settings.py
+++ project/config/settings.py
@@ -144,5 +144,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# Funcionamiento para subir archivos de media
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/admin"
