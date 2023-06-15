9ecaf2478afe00dd637d47593ee30e4913252eb1
Martin Naveiro <martin.naveiro@gmail.com>
2023-06-15 02:31:45 -0300

    buscar

--- project/apps/venta/urls.py
+++ project/apps/venta/urls.py
@@ -6,10 +6,12 @@ from . import views
 from producto.views import mostrar_productos
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import buscar_productos
 
 app_name = "venta"
 
 urlpatterns = [
     path("productos/", views.mostrar_productos, name="mostrar_ventas"),
     path("mostrar/", views.mostrar_productos, name="mostrar_productos"),
+    path("buscar/", buscar_productos, name="buscar_productos"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- project/apps/venta/views.py
+++ project/apps/venta/views.py
@@ -8,8 +8,23 @@
 
 from django.shortcuts import render
 from producto.models import Producto
+from django.shortcuts import render
+from producto.models import Producto
+from django.db.models import Q
 
 
 def mostrar_productos(request):
     productos = Producto.objects.all()
     return render(request, "venta/index.html", {"productos": productos})
+
+
+def buscar_productos(request):
+    term = request.GET.get("q")
+    productos = Producto.objects.filter(
+        Q(tipo_vehiculo__icontains=term)
+        | Q(marca__icontains=term)
+        | Q(modelo__icontains=term)
+        | Q(anio__icontains=term)
+    )
+    context = {"productos": productos, "term": term}
+    return render(request, "venta/lista_productos.html", context)
